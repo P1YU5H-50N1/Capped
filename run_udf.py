9b83e8343640fad9f85284745b59e288af0d63ba
PiyushAtiRage <piyush.s@irage.in>
2025-01-08 18:43:44 +0530

    .

--- run_udf.py
+++ run_udf.py
@@ -1,9 +1,21 @@
 import polars as pl
-from capped import capped_cum_sum
+from capped import capped_cum_sum as custom_capped_cum_sum
+
+@pl.api.register_expr_namespace("lmt")
+class bounds:
+    def __init__(self, expr: pl.Expr) -> None:
+        self._expr = expr
+
+    def capped_cum_sum(self,cap) -> pl.Expr:
+        return custom_capped_cum_sum(self._expr,cap=cap)
+
 
 df = pl.DataFrame({
-    "values": [1,0,1,-1,-1,0,0,0,-1,1,1,1,0,0,0]
+    "values": [5,5,5,12,-16,-3,4,5,6,-10]
 })
-result = df.with_columns(result=capped_cum_sum('values',cap=12))
+# result = df.with_columns(result=capped_cum_sum('values',cap=12))
+result = df.with_columns(
+    pl.col('values').lmt.capped_cum_sum(cap=12).alias('result')
+)
 
 print(result)
